e3b66abea1c0edb03e741b8d750ef8ed92e405d1
AndreR <andre@ryll.cc>
2026-04-12 16:22:25 +0000

    Add systemd service and buildroot package files

---
+++ ssl-tracking-cam.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# ssl-tracking-cam
+#
+################################################################################
+
+SSL_TRACKING_CAM_SITE = $(BR2_EXTERNAL_TIGERS_PATH)/package/cam
+SSL_TRACKING_CAM_SITE_METHOD = local
+SSL_TRACKING_CAM_DEPENDENCIES = systemd libgpiod2 protobuf eigen boost
+SSL_TRACKING_CAM_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS = --include .git
+SSL_TRACKING_CAM_SUPPORTS_IN_SOURCE_BUILD = NO
+
+$(eval $(cmake-package))
