92b36c8cf821760eab8c3f18e435d3075a35e7b3
AndreR <andre@ryll.cc>
2026-04-14 23:01:13 +0000

    Fix CI build

--- ssl-tracking-cam.mk
+++ ssl-tracking-cam.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SSL_TRACKING_CAM_SITE = $(BR2_EXTERNAL_TIGERS_PATH)/package/cam
+SSL_TRACKING_CAM_SITE = $(BR2_EXTERNAL_TIGERS_PATH)/package/ssl-tracking-cam
 SSL_TRACKING_CAM_SITE_METHOD = local
 SSL_TRACKING_CAM_DEPENDENCIES = systemd libgpiod2 protobuf eigen boost
 SSL_TRACKING_CAM_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS = --include .git
